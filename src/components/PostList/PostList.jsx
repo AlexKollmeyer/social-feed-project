cc813058b944e50fc3ea7d6bf1ebb84a83784eeb
Alex Kollmeyer <akollmeyer123@gmail.com>
2023-09-11 13:51:42 -0500

    Created all components, finshed the jsx of the header, starting to build out the rest, will connect them later.

---
+++ src/components/PostList/PostList.jsx
@@ -0,0 +1,26 @@
+const PostList = ({}) => {
+  return (
+    <div>
+      <div>
+        <h4>Poster's Name</h4>
+        <p>Text in Post</p>
+        <button>Like</button>
+        <button>Dislike</button>
+      </div>
+      <div>
+        <h4>Poster's Name</h4>
+        <p>Text in Post</p>
+        <button>Like</button>
+        <button>Dislike</button>
+      </div>
+      <div>
+        <h4>Poster's Name</h4>
+        <p>Text in Post</p>
+        <button>Like</button>
+        <button>Dislike</button>
+      </div>
+    </div>
+  );
+};
+
+export default PostList;
